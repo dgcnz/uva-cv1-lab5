89a71bb4f5e49aa996757e8616050d35bef6cdaf
dqmis <dom.seputis@gmail.com>
2023-10-10 12:38:01 +0200

    Adding early stopping

---
+++ src/training/early_stopper.py
@@ -0,0 +1,15 @@
+class EarlyStopper:
+    def __init__(self, patience: int = 7, delta: float = 0.0):
+        self._patience = patience
+        self._delta = delta
+        self.losses = []
+
+    def stop(self):
+        if len(self.losses) < self._patience:
+            return False
+
+        for i in range(1, self._patience + 1):
+            if self.losses[-i] - self.losses[-i - 1] > self._delta:
+                return False
+
+        return True
